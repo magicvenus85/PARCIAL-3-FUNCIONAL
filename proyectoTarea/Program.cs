d4aac75261e35da66e91da75c80b17613b019f81
marito <167055786+magicvenus85@users.noreply.github.com>
2024-06-05 18:58:28 -0600

    Add files via upload
    Ci

---
+++ proyectoTarea/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace JuegoSudoku
+{
+    class Program
+    {
+        static void Main()
+        {
+            Juego juego = new Juego();
+            juego.Iniciar();
+        }
+    }
+}
